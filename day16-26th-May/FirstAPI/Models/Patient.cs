3eedb6e0f28377edead6c8f953d38e32181126e0
KaushikKannanB <kaushik0fficial3009@gmail.com>
2025-05-26 16:01:37 +0530

    webapi - CRUD for Doctors and Patients

---
+++ day16-26th-May/FirstAPI/Models/Patient.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FirstAPI.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int age { get; set; }
+
+    }
+}
