641e0be603772665f504381c924d135a350e6094
KaushikKannanB <kaushik0fficial3009@gmail.com>
2025-06-11 15:48:34 +0530

    api calls-parent and child component

---
+++ day28-11th-Jun/myApp/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { First } from "./first/first";
+
+import { Products } from './products/products';
+import { Recipe } from './recipe/recipe';
+import { Recipes } from './recipes/recipes';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  imports: [Recipes]
+})
+export class App {
+  protected title = 'myApp';
+}
